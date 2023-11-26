9d5a93bff891783921aaab078542a0fc9daf1b1c
khaled <lsnoop75@gmail.com>
2023-11-26 19:48:34 +0100

    - some responsive design, "Developer" background

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 console.log("test");
+
+const openItemsBtn = document.getElementById("open__items-btn");
+
+const navItems = document.getElementById("nav__items");
+
+document.onclick = function (e) {
+  if (e.target.id !== "nav__items" && e.target.id !== "open__items-btn") {
+    openItemsBtn.classList.remove("active");
+    navItems.classList.remove("active");
+  }
+};
+
+openItemsBtn.onclick = function () {
+  openItemsBtn.classList.toggle("active");
+  navItems.classList.toggle("active");
+};
